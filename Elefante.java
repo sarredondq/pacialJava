51eb3d5e150c3876b17df06562c7c303464e59fe
sarredondq <sarredondq@eafit.edu.co>
2018-10-11 15:08:51 -0500

    Archivos

---
+++ Elefante.java
@@ -0,0 +1,28 @@
+public class Elefante{
+    private int peso;
+    private int ci;
+    private int pocision;
+    public void Elefante(int peso,int ci,int pocision){
+        this.peso = peso;
+        this.ci = ci;
+        this.pocision = pocision;
+    }
+    public void setPeso(int peso){
+        this.peso = peso;
+    }
+    public void setCi(int ci){
+        this.ci = ci;
+    }
+    public void setPocision(int pocision){
+        this.pocision = pocision;
+    }
+    public int getPeso(){
+        this.peso;
+    }
+    public int getCi(){
+        this.ci;
+    }
+    public int getPosicion(){
+        this.pocision;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Elefante e1 = new Elefante(1000,2000,1);
+        System.out.println(e1);
+    }
+}

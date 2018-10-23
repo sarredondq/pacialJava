7eafc0047b78d28106982f17f998b619b364274f
sarredondq <sarredonq@eafit.edu.co>
2018-10-23 12:26:07 -0500

    parte

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
-public class Main{
-    public static void main(String[] args) {
-        Elefante e1 = new Elefante(1000,2000,1);
-        System.out.println(e1);
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int i = 1;
+        String s;
+        Scanner teclado = new Scanner(System.in);
+        ArrayList<Elefante> listaElefante = new ArrayList<Elefante>();		
+		while (true) {
+            int s1;
+            int s2;
+			s = teclado.nextLine();
+			if (s==null || s.equals("")) {
+				break;
+            }
+            Scanner scan = new Scanner(s);
+            s1 = scan.nextInt();
+            s2 = scan.nextInt();
+            Elefante tem = new Elefante(s1, s2, i);
+            listaElefante.add(tem);
+            i++;
+            scan.close();
+        }
+        teclado.close();
+        System.out.println(listaElefante.get(0).getPeso());
+        for(Elefante j : listaElefante){
+            System.out.println(j.getPeso());
+        }
     }
 }

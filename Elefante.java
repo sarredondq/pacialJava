5cf8eb87c441a434b2d728096c63b4e41f036787
sarredondq <sarredonq@eafit.edu.co>
2018-11-04 17:08:52 -0500

    parte del parcial

--- Elefante.java
+++ Elefante.java
@@ -1,28 +1,42 @@
-public class Elefante{
+
+public class Elefante implements Comparable<Elefante> {
+
     private int peso;
     private int ci;
     private int pocision;
-    public Elefante(int peso,int ci,int pocision){
+
+    public Elefante(int peso, int ci, int pocision) {
         this.peso = peso;
         this.ci = ci;
         this.pocision = pocision;
     }
-    public void setPeso(int peso){
+
+    public void setPeso(int peso) {
         this.peso = peso;
     }
-    public void setCi(int ci){
+
+    public void setCi(int ci) {
         this.ci = ci;
     }
-    public void setPocision(int pocision){
+
+    public void setPocision(int pocision) {
         this.pocision = pocision;
     }
-    public int getPeso(){
+
+    public int getPeso() {
         return this.peso;
     }
-    public int getCi(){
+
+    public int getCi() {
         return this.ci;
     }
-    public int getPosicion(){
+
+    public int getPosicion() {
         return this.pocision;
+    }
+
+    @Override
+    public int compareTo(Elefante o) {
+        return this.peso - o.peso;
     }
 }
--- Main.java
+++ Main.java
@@ -1,48 +1,64 @@
-import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author santiago
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    /** metodo main */
-    public static void main(String[] args) {
+
+    /**
+     * metodo main
+     *
+     * @param args
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
         // ingresar los datos de los elefantes y ponerlos en una lista
         int i = 1;
         String s;
-        Scanner teclado = new Scanner(System.in);
-        ArrayList<Elefante> listaElefante = new ArrayList<Elefante>();
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Elefante> listaElefante = new ArrayList<>();
         while (true) {
             int s1;
             int s2;
-            s = teclado.nextLine();
+            s = teclado.readLine();
             if (s == null || s.equals("")) {
                 break;
             }
-            Scanner scan = new Scanner(s);
-            s1 = scan.nextInt();
-            s2 = scan.nextInt();
+            StringTokenizer scan = new StringTokenizer(s);
+            s1 = Integer.parseInt(scan.nextToken());
+            s2 = Integer.parseInt(scan.nextToken());
             Elefante tem = new Elefante(s1, s2, i);
             listaElefante.add(tem);
             i++;
-            scan.close();
         }
-        teclado.close();
+        Elefante[] arrPorPeso = listaElefante.toArray(new Elefante[listaElefante.size()]);
+        Arrays.sort(arrPorPeso);
+        int[] lisTemp = new int[arrPorPeso.length];
+        lisTemp[0] = 1;
 
-        while (!(listaElefante.isEmpty())) {
-            // menor peso
-            Elefante elMenorPeso = listaElefante.get(0);
-            for (Elefante j : listaElefante) {
-                if(elMenorPeso.getPeso() > j.getPeso()){
-                    elMenorPeso = j;
+        for (int j = 1; j < lisTemp.length; j++) {
+            for (int k = 0; k < j; k++) {
+                if (arrPorPeso[j].getCi() < arrPorPeso[k].getCi() && arrPorPeso[j].getPeso() > arrPorPeso[k].getPeso()) {
+                    lisTemp[j] = Math.max(lisTemp[k] + 1, lisTemp[j]);
                 }
             }
-            Elefante elMayorCi = elMenorPeso;
-
-            for (Elefante j : listaElefante) {
-                if(elMayorCi.getCi() < j.getCi()){
-                    elMayorCi = j;
-                }
-            }
-            
         }
+        int max = 0;
+        for (int j : lisTemp) {
+            max = Math.max(j, max);
+        }
+        System.out.println(max);
     }
 }
